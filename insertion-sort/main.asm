3df08619d30084ad3042e5ffaf90c294ae5256d3
Matthew Kandiah <mj.kandiah@googlemail.com>
2026-09-02 08:11:04 +0100

    pass arguments to function - now it's reusable

--- insertion-sort/main.asm
+++ insertion-sort/main.asm
@@ -6,38 +6,48 @@
 	
 	PRINT orig, orig_size
 	PRINT nums, nums_size
-
+	ARG1 nums
+	ARG2 nums_size
 	call do_sort
-		
 	PRINT sort, sort_size
 	PRINT nums, nums_size
 
+	PRINT orig, orig_size
+	PRINT nums2, nums2_size
+	ARG1 nums2
+	ARG2 nums2_size
+	call do_sort
+	PRINT sort, sort_size
+	PRINT nums2, nums2_size
+	
 	EXIT 0
 
 do_sort:
-	;; sort in place
+	;; rdi: nums
+	;; rsi: nums_size
 	;; r8 = j
 	;; r9 = key
 	;; r10 = i
 	mov r8, 1
+	dec rsi
 .begin:
-	cmp r8, nums_size-1
+	cmp r8, rsi
 	jge .stop
 
-	movzx r9, byte [nums + r8]
+	movzx r9, byte [rdi + r8]
 	mov r10, r8
 	dec r10
 .inner:
 	cmp r10, 0
 	jl .insert
-	cmp byte [nums + r10], r9b
+	cmp byte [rdi + r10], r9b
 	jle .insert
-	mov al, byte [nums + r10]
-	mov byte [nums + r10 + 1], al
+	mov al, byte [rdi + r10]
+	mov byte [rdi + r10 + 1], al
 	dec r10
 	jmp .inner
 .insert:
-	mov byte [nums + r10 + 1], r9b
+	mov byte [rdi + r10 + 1], r9b
 	inc r8
 	jmp .begin
 .stop:
@@ -51,3 +61,5 @@ do_sort:
 	sort_size = $-sort
     	nums file 'data.txt'
 	nums_size = $-nums
+	nums2 file 'data2.txt'
+	nums2_size = $-nums2
