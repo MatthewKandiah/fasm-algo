49bcc92bcdf3ff8cae042de27db8b4ef985401d7
Matthew Kandiah <mj.kandiah@googlemail.com>
2026-09-02 07:51:03 +0100

    split out a function

--- insertion-sort/main.asm
+++ insertion-sort/main.asm
@@ -7,33 +7,42 @@
 	PRINT orig, orig_size
 	PRINT nums, nums_size
 
+	call do_sort
+		
+	PRINT sort, sort_size
+	PRINT nums, nums_size
+
+	EXIT 0
+
+;; TODO use caller-saved registers
+do_sort:
 	;; sort in place
 	;; rbx = j
 	;; rcx = key
 	;; rdx = i
 	mov rbx, 1
-begin:	cmp rbx, nums_size-1
-	jge stop
+.begin:
+	cmp rbx, nums_size-1
+	jge .stop
 
 	movzx rcx, byte [nums + rbx]
 	mov rdx, rbx
 	dec rdx
-inner: 	cmp rdx, 0
-	jl insert
+.inner:
+	cmp rdx, 0
+	jl .insert
 	cmp byte [nums + rdx], cl
-	jle insert
+	jle .insert
 	mov al, byte [nums + rdx]
 	mov byte [nums + rdx + 1], al
 	dec rdx
-	jmp inner
-insert:	mov byte [nums + rdx + 1], cl
+	jmp .inner
+.insert:
+	mov byte [nums + rdx + 1], cl
 	inc rbx
-	jmp begin
-	
-stop:	PRINT sort, sort_size
-	PRINT nums, nums_size
-
-exit:	EXIT 0
+	jmp .begin
+.stop:
+	ret
 	
 	segment readable writable
 
